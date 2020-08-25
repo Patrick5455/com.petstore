3f6f3ff9cf50647088a3b80914c3c033f9e581b1
Patrick5455 <patrickojunde@gmail.com>
2020-08-25 12:36:17 +0100

    Added annotaion for Repo Test classes

--- src/test/java/com/petstore/petstoreapp/repository/StoreRepoTest.java
+++ src/test/java/com/petstore/petstoreapp/repository/StoreRepoTest.java
@@ -2,9 +2,11 @@ package com.petstore.petstoreapp.repository;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest // this annotation marks the class as a test class, it also provides the beans of the container from the main application available during test
 class StoreRepoTest {
 
     @BeforeEach
